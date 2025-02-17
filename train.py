06559e3a14fbbd4c04d048b25c4061cbdaaebf5e
Kumar SS <79216521+kumar-ss@users.noreply.github.com>
2025-02-17 14:47:52 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,23 +5,25 @@ import socket
 import yaml
 import torch
 import torch.backends.cudnn as cudnn
+import torch.utils.data
 import numpy as np
 import torchvision
 import models
 import datasets
 import utils
-from models import DenoisingDiffusion, DiffusiveRestoration
+from models import DenoisingDiffusion
 
 
 def parse_args_and_config():
-    parser = argparse.ArgumentParser(description='Evaluate Wavelet-Based Diffusion Model')
+    parser = argparse.ArgumentParser(description='Training Wavelet-Based Diffusion Model')
     parser.add_argument("--config", default='LOLv1.yml', type=str,
                         help="Path to the config file")
-   
-    parser.add_argument('--resume', default='/kaggle/input/lolv2synthetic/LOLv2/ckpt', type=str,
+    #parser.add_argument('--resume', default='/content/drive/My Drive/LOLI/LOLv2/ckpt', type=str,
+    parser.add_argument('--resume', default='', type=str,
                         help='Path for checkpoint to load and resume')
     parser.add_argument("--sampling_timesteps", type=int, default=10,
                         help="Number of implicit sampling steps for validation image patches")
+    #parser.add_argument("--image_folder", default='/content/drive/My Drive/LOLI/LOLv2/results/', type=str,
     parser.add_argument("--image_folder", default='results/', type=str,
                         help="Location to save restored validation image patches")
     parser.add_argument('--seed', default=230, type=int, metavar='N',
@@ -54,9 +56,6 @@ def main():
     print("Using device: {}".format(device))
     config.device = device
 
-    if torch.cuda.is_available():
-        print('Note: Currently supports evaluations (restoration) when run only on a single GPU!')
-
     # set random seed
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
@@ -65,17 +64,14 @@ def main():
     torch.backends.cudnn.benchmark = True
 
     # data loading
-    print("=> using dataset '{}'".format(config.data.val_dataset))
+    print("=> using dataset '{}'".format(config.data.train_dataset))
     DATASET = datasets.__dict__[config.data.type](config)
-    # Removed parse_patches=False from here
-    _, val_loader = DATASET.get_loaders()  # This no longer takes parse_patches argument
 
     # create model
-    print("=> creating denoising-diffusion model")
+    print("=> creating denoising-diffusion model...")
     diffusion = DenoisingDiffusion(args, config)
-    model = DiffusiveRestoration(diffusion, args, config)
-    model.restore(val_loader)
+    diffusion.train(DATASET)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
