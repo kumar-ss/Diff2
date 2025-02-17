ceac6668ba23c18ef8548c8106274865517091c3
Kumar SS <79216521+kumar-ss@users.noreply.github.com>
2025-02-17 14:49:07 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -66,7 +66,8 @@ def main():
     # data loading
     print("=> using dataset '{}'".format(config.data.val_dataset))
     DATASET = datasets.__dict__[config.data.type](config)
-    _, val_loader = DATASET.get_loaders(parse_patches=False)
+    # Removed parse_patches=False from here
+    _, val_loader = DATASET.get_loaders()  # This no longer takes parse_patches argument
 
     # create model
     print("=> creating denoising-diffusion model")
