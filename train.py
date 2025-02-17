7ea2e04de8792e8fbd52fe2512b8658a2f92b456
Kumar SS <79216521+kumar-ss@users.noreply.github.com>
2025-02-17 14:48:26 +0530

    Update train.py

--- train.py
+++ train.py
@@ -18,12 +18,12 @@ def parse_args_and_config():
     parser = argparse.ArgumentParser(description='Training Wavelet-Based Diffusion Model')
     parser.add_argument("--config", default='LOLv1.yml', type=str,
                         help="Path to the config file")
-    #parser.add_argument('--resume', default='/content/drive/My Drive/LOLI/LOLv2/ckpt', type=str,
+    #parser.add_argument('--resume', default='/kaggle/input/lolv2synthetic/LOLv2/ckpt', type=str,
     parser.add_argument('--resume', default='', type=str,
                         help='Path for checkpoint to load and resume')
     parser.add_argument("--sampling_timesteps", type=int, default=10,
                         help="Number of implicit sampling steps for validation image patches")
-    #parser.add_argument("--image_folder", default='/content/drive/My Drive/LOLI/LOLv2/results/', type=str,
+    #parser.add_argument("--image_folder", default='/kaggle/input/lolv2synthetic/LOLv2/results/', type=str,
     parser.add_argument("--image_folder", default='results/', type=str,
                         help="Location to save restored validation image patches")
     parser.add_argument('--seed', default=230, type=int, metavar='N',
