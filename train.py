ad482396491d9362486e3b467dd75380eb2e0d93
Kumar SS <79216521+kumar-ss@users.noreply.github.com>
2025-02-17 14:42:39 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,17 +5,16 @@ import socket
 import yaml
 import torch
 import torch.backends.cudnn as cudnn
-import torch.utils.data
 import numpy as np
 import torchvision
 import models
 import datasets
 import utils
-from models import DenoisingDiffusion
+from models import DenoisingDiffusion, DiffusiveRestoration
 
 
 def parse_args_and_config():
-    parser = argparse.ArgumentParser(description='Training Wavelet-Based Diffusion Model')
+    parser = argparse.ArgumentParser(description='Evaluate Wavelet-Based Diffusion Model')
     parser.add_argument("--config", default='LOLv1.yml', type=str,
                         help="Path to the config file")
    
@@ -55,6 +54,9 @@ def main():
     print("Using device: {}".format(device))
     config.device = device
 
+    if torch.cuda.is_available():
+        print('Note: Currently supports evaluations (restoration) when run only on a single GPU!')
+
     # set random seed
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
@@ -63,14 +65,17 @@ def main():
     torch.backends.cudnn.benchmark = True
 
     # data loading
-    print("=> using dataset '{}'".format(config.data.train_dataset))
+    print("=> using dataset '{}'".format(config.data.val_dataset))
     DATASET = datasets.__dict__[config.data.type](config)
+    # Removed parse_patches=False from here
+    _, val_loader = DATASET.get_loaders()  # This no longer takes parse_patches argument
 
     # create model
-    print("=> creating denoising-diffusion model...")
+    print("=> creating denoising-diffusion model")
     diffusion = DenoisingDiffusion(args, config)
-    diffusion.train(DATASET)
+    model = DiffusiveRestoration(diffusion, args, config)
+    model.restore(val_loader)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
